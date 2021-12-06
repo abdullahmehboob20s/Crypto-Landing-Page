d788f169b7b579d21b32fc15ae2a252d23544f24
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2021-12-06 14:06:38 +0500

    as

--- src/layouts/Wrapper/Wrapper.js
+++ src/layouts/Wrapper/Wrapper.js
@@ -1,10 +1,17 @@
 import Footer from "layouts/Footer/Footer";
 import Navbar from "layouts/Navbar/Navbar";
-import React from "react";
+import React, { useEffect } from "react";
+import { useRef } from "react";
 
 function Wrapper(props) {
+  const wrapperRef = useRef();
+
+  useEffect(() => {
+    wrapperRef.current.style.minHeight = window.innerHeight + "px";
+  });
+
   return (
-    <div className="wrapper bg-dark-purple">
+    <div className="wrapper bg-dark-purple" ref={wrapperRef}>
       <div className="wrapper-body mb-20px">
         <Navbar />
 
